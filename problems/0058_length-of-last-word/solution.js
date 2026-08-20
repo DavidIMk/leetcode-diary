7a6a06a4d28e541f2c630a63a20f03c9f7d40b40
David <davidimmanuelmaksi@gmail.com>
2026-08-20 22:18:56 +0700

    Solve Length of Last Word

---
+++ problems/0058_length-of-last-word/solution.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  const arr = s.trim().split(" ");
+  const lastWord = arr[arr.length - 1];
+  return lastWord.length;
+};
